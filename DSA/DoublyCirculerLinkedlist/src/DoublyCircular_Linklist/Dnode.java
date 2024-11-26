2034d039955dafb30d293f7a7cbdbba8d89084d5
Ruturaj Kamble <155933375+Ruturajkamble23@users.noreply.github.com>
2024-11-27 01:51:41 +0530

    Add files via upload

---
+++ DSA/DoublyCirculerLinkedlist/src/DoublyCircular_Linklist/Dnode.java
@@ -0,0 +1,12 @@
+package DoublyCircular_Linklist;
+
+public class Dnode {
+    int data;
+    Dnode left,right;
+    Dnode(int data)
+    {
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+}
---
+++ DSA/DoublyCirculerLinkedlist/src/DoublyCircular_Linklist/DoublyCircular.java
@@ -0,0 +1,86 @@
+package DoublyCircular_Linklist;
+
+public class DoublyCircular {
+    Dnode root;
+
+
+    void create_List() {
+        root = null;
+    }
+    void insert_left(int data)
+    {
+        Dnode n=new Dnode(data);
+        if(root==null)
+            root=n;//first
+        else
+        {
+            n.right=root;//1
+            root.left=n;//2
+            root=n; //3
+        }
+        System.out.println(data+" inserted");
+    }
+    void delete_left()
+    {
+
+        if(root==null)
+            System.out.println("\nList Empty");
+        else
+        {
+            Dnode t=root;//1
+            root=root.right;//2
+            root.left=null;//3
+            System.out.println(t.data+" deleted");//3
+        }
+
+    }
+
+    void insert_right(int data)
+    {
+        Dnode n=new Dnode(data);
+        if(root==null)
+            root=n;//first
+        else
+        {
+            Dnode t=root;//1
+            while(t.right!=null)//2
+                t=t.right;
+            t.right=n;//3
+            n.left=t;//4
+        }
+        System.out.println(data+" inserted");
+
+    }
+    void delete_right()
+    {
+        if(root==null)
+            System.out.println("\nList Empty");
+        else
+        {Dnode t=root;//1
+            while(t.right!=null)//2
+                t=t.right;
+            if(root==t)
+                root=null;//3 single node
+            else {
+                Dnode t2 = t.left;//3
+                t2.right = null;//4
+            }
+            System.out.println(t.data+" deleted");//3
+        }
+    }
+
+    void print_list()
+    {
+        if(root==null)
+            System.out.println("\nList Empty");
+        else
+        {
+            Dnode t=root;//1
+            while(t!=null)//2
+            {
+                System.out.print("<-|"+t.data+"|->");
+                t=t.right;
+            }
+        }
+    }
+}
