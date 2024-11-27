67c494732e5003ec624edd40fdfaf16b3e6c0c55
Ruturaj Kamble <155933375+Ruturajkamble23@users.noreply.github.com>
2024-11-27 23:39:05 +0530

    Add files via upload

---
+++ DSA/EmployeeLinkedlist/Employee.java
@@ -0,0 +1,15 @@
+package EmployeeLinkedlist;
+
+public class Employee {
+    int employeeId;
+    String employeeName;
+    String gender;
+    int salary;
+
+    public Employee(int employeeId, String employeeName, String gender, int salary) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.gender = gender;
+        this.salary = salary;
+    }
+}
---
+++ DSA/EmployeeLinkedlist/Node.java
@@ -0,0 +1,12 @@
+package EmployeeLinkedlist;
+
+public class Node {
+    Employee employee;
+    Node next;
+    public Node(Employee employee)
+    {
+        this.employee=employee;
+        this.next=null;
+
+    }
+}
