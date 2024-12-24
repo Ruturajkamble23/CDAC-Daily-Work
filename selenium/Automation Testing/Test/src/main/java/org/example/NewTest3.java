60a8aaf3dbfcbc82cd7ef5f11f78922ba6ee7645
Ruturaj Kamble <155933375+Ruturajkamble23@users.noreply.github.com>
2024-12-25 02:15:09 +0530

    Add files via upload

---
+++ selenium/Automation Testing/Test/src/main/java/org/example/NewTest3.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.By;
+public class NewTest3 {
+    public static WebDriver dr;
+    @Test
+    public void f() {
+
+    }
+    @BeforeTest
+    public void beforeTest() {
+        dr =  new ChromeDriver();
+        String url ="https://www.saucedemo.com/v1/";
+        dr.get(url);
+        System.out.println("Open");
+    }
+
+    @Test(priority=1)
+    public void afterTest() {
+        // throw new SkipException("Error");
+        dr.findElement(By.id("user-name")).sendKeys("problem_user");
+
+    }
+
+    // @Test(priority=2, enabled=false)
+    @Test(priority=2)
+    public void Password() {
+        dr.findElement(By.id("password")).sendKeys("secret_sauce");
+
+    }
+
+    @Test(priority=3)
+    public void Login() {
+        // throw new SkipException("Error");
+        dr.findElement(By.id("login-button")).click();
+        System.out.println("All Test Completed...");
+    }
+
+
+}
+
